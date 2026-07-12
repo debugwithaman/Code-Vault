e7ef727e5065bfd381e9d108e972df96c8cd5799
Aman Kazi <debugwithkazi@gmail.com>
2026-07-13 00:42:23 +0530

    Time: 17 ms (30.05%), Space: 12.5 MB (36.97%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -9,17 +9,17 @@ public:
 
         for(high =0;high<n ;high++){
             f[s[high]]++;
-            int k = high-low+1;
+            int len = high-low+1;
 
-            while(f.size() < k){
+            while(f.size() < len){
                 f[s[low]]--;
                 if(f[s[low]] == 0)
                   f.erase(s[low]);
                 low++;
-                k = high - low + 1;
+                len = high - low + 1;
 
             }
-            int len = high - low + 1;
+            len = high - low + 1;
             res = max(res,len);
         }
         if(s == ""){
