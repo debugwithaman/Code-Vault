e3362d6de5ae909eb71c01b8b9732e226b062520
Aman Kazi <debugwithkazi@gmail.com>
2026-07-28 23:32:51 +0530

    Time: 80 ms (17.88%), Space: 111.5 MB (14.96%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -3,16 +3,13 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_map<int,int> map;
 
-       for(int x : nums){
-         map[x]++;
-       }
+        for(int x : nums){
+            if(map.find(x) != map.end()){
+                return true;
+            }
+            map[x]++;
+        }
 
-       for(auto it : map){
-          if(it.second > 1){
-            return true;
-          }
-       }
         return false;
-        
     }
 };
